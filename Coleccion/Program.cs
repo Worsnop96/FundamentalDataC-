5de8b77aa9818723df7a8a655f2b880276b351ea
Dylan Estrada <dylan@hotmail.com>
2021-10-22 15:59:49 -0500

    agregar colexion

---
+++ Coleccion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coleccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numeros = new Queue<int>();
+            foreach(int numero in new int[5] { 5, 4, 3, 6, 9 })
+            {
+                numeros.Enqueue(numero); 
+            }
+        }
+    }
+}
