22aba383c01d5e88a3f7657acaae1fda9d2adc40
Dylan Estrada <dylan@hotmail.com>
2021-10-21 21:16:09 -0500

    First commit Array

---
+++ Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] database = LeerDatos();
+            foreach (int i in database)  Console.WriteLine(i);
+
+        }
+
+
+        static int[] LeerDatos()
+        {
+            Console.WriteLine("how many elements do you want ?");
+            string respuesta = Console.ReadLine();
+            int numElementos = int.Parse(respuesta);
+            int[] datos = new int[numElementos];
+
+            for (int i= 0; i < numElementos; i++)
+            {
+                Console.WriteLine($"introduce el dato para la posicion {i}");
+                respuesta = Console.ReadLine();
+                int datosElemento = int.Parse(respuesta);
+                datos[i] = datosElemento;
+            }
+            return datos;
+        }
+        
+        
+    }
+
+
+}
